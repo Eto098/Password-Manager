0bc54b580ecc414f56cca8d266cc85a48d54edcf
melorthys <76265731+melorthys@users.noreply.github.com>
2021-05-13 23:39:52 +0300

    added white eye icon for password toggles

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-      frame: false,
+    frame: true,
     width: 1024,
     height: 720,
     webPreferences: {
